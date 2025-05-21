baf057ec0e696ede65ba609fed759270a4741241
Ajay Kumar Nandivada <ajaykumarnandivada@gmail.com>
2025-05-21 15:01:45 +0530

    added development dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState("a new note..")
   const [showAll, setShowAll] = useState(true)
   const handleNoteChange = (event) => {
@@ -20,6 +21,18 @@ const App = (props) => {
     setNewNote('')
   }
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then((response) => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
   return (
     <div>
       <h1>Notes</h1>
